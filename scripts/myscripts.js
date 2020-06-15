ff08bbb98095c88a680844786656f8624a9e387d
VFagionato <viktormfagionato@gmail.com>
2020-06-15 18:02:33 -0300

    salvando estrutura do html, css e js. --- Criada a função colorize para randomizar as cores de fundo do jogo da memoria -- adicionado algoritimo para embaralhar as cores de fundo (pego da net)

---
+++ scripts/myscripts.js
@@ -0,0 +1,34 @@
+const cardsNode = document.querySelectorAll('div.carta')
+const cardsArr = Array.from(cardsNode)
+let cores = ['#BD201B', '#C7AC26', '#B31FB2', '#2F1CC7', '#3396BD', '#C75724', '#28BD19', '#fff']
+let cores2 = ['#BD201B', '#C7AC26', '#B31FB2', '#2F1CC7', '#3396BD', '#C75724', '#28BD19', '#fff']
+
+
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+function colorize(){
+    shuffleArray(cores)
+    shuffleArray(cores2)
+
+    let bg1 = 0
+    let bg2 = 8
+
+    for (let pos = 0 ; pos < cores.length ; pos++){
+        cardsArr[bg1].style.backgroundColor = cores[pos]
+        bg1++
+    }
+
+    for (let pos1 = 0 ; pos1 < cores2.length ; pos1++){
+        cardsArr[bg2].style.backgroundColor = cores2[pos1]
+        bg2++
+    }
+}
+
