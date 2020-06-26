3c03fb7fe5f1f9da36b554e125d623f4d08eae3a
VFagionato <viktormfagionato@gmail.com>
2020-06-25 22:43:33 -0300

    resolvido o bug da função flip, adicionada a função novoJogo no botão ---- próximos passos: adicionar sistema de pontuação e ranking

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -2,6 +2,7 @@ const cardsNode = document.querySelectorAll('div.conteudocarta')
 const cardsArr = Array.from(cardsNode)
 let cores = ['#BD201B', '#C7AC26', '#B31FB2', '#2F1CC7', '#3396BD', '#C75724', '#28BD19', '#fff', '#BD201B', '#C7AC26', '#B31FB2', '#2F1CC7', '#3396BD', '#C75724', '#28BD19', '#fff']
 const divFlip = document.getElementsByClassName('flipper')
+const divArr = Array.from(divFlip)
 let verify = []
 
 /* Randomize array in-place using Durstenfeld shuffle algorithm */
@@ -17,7 +18,6 @@ function shuffleArray(array) {
 function colorize(){
     shuffleArray(cores)
     let bg1 = 0
-    let bg2 = 8
     for (let pos = 0 ; pos < cores.length ; pos++){
         cardsArr[bg1].style.backgroundColor = cores[pos]
         bg1++
@@ -25,13 +25,14 @@ function colorize(){
     for (let i = 0; i < divFlip.length ; i++){
         divFlip[i].addEventListener("click", flip)
     }
-    shuffleArray(cardsArr)
 }
 
 
 function flip(){
     if(this.style.transform == "rotateY(180deg)"){
-        alert('[erro] Por favor, selecione outra carta')
+        alert('Selecione somente 2 cartas diferentes')
+    } else if (verify.length >= 2){
+        alert('Por favor, espere a validação')
     } else {
         this.style.transform = "rotateY(180deg)"
         if (verify.length >= 1){
@@ -43,6 +44,7 @@ function flip(){
     }
 }
 
+
 function teste(){
     if ( verify[0].children[1].style.backgroundColor == verify[1].children[1].style.backgroundColor){
         verify[0].style.opacity = "0%"
@@ -53,4 +55,16 @@ function teste(){
         verify[1].style.transform = "rotateY(0deg)"
         verify = []
     }
+}
+
+
+
+function novoJogo(){
+    for (let i = 0; i < divFlip.length ; i++){
+        divFlip[i].style.transform = "rotateY(0deg)"
+        divFlip[i].style.opacity = "100%"
+    }
+    verify = []
+    setTimeout("colorize()", 1000)
+    
 }
